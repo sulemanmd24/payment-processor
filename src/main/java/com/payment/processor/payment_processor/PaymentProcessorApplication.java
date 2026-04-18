f6f1f0244c95b5cb42e82832d72f81341e105a42
sulemanmd24 <sulemanmd.04@gmail.com>
2026-04-18 14:17:04 +0530

    Initial commit (#1)
    Co-authored-by: Md Suleman <msuleman@wsgc.com>

---
+++ src/main/java/com/payment/processor/payment_processor/PaymentProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.payment.processor.payment_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payment/processor/payment_processor/PaymentProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.processor.payment_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
